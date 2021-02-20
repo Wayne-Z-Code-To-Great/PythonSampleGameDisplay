70ab0eca4de767199852d8eeca7654dbd3159a34
Wayne Zheng <wnz6klj@gmail.com>
2021-02-20 00:17:00 -0500

    Revised the code so that there is track of score, when the bird hit the edge or the blocking bar, it resets the game to initial conditions.

--- flappyBird.py
+++ flappyBird.py
@@ -13,22 +13,41 @@ bar_up.y = 0
 bar_down = Actor('bar_down')
 bar_down.x = 300
 bar_down.y = 600
+score = 0
+speed = 1
 
 def draw():
     background.draw()
     bar_up.draw()
     bar_down.draw()
     bird.draw()
+    screen.draw.text(str(score), (30, 30), 
+                    fontsize=50, color='green')
 
 def update():
+    global score, speed
     bird.y = bird.y + 2
-    bar_up.x = bar_up.x - 2
-    bar_down.x = bar_down.x - 2
+    bar_up.x = bar_up.x - speed
+    bar_down.x = bar_down.x - speed
     if bar_up.x < 0:
         bar_up.x = WIDTH
         bar_down.x = WIDTH
         bar_up.y = random.randint(-200, 200)
         bar_down.y = 600 + bar_up.y
+        score = score + 1
+        if (score % 5 ==0):
+            speed = speed + 1
+    if bird.colliderect(bar_up) or bird.colliderect(bar_down) \
+                or bird.y<0 or bird.y>HEIGHT:
+        print('Failed')
+        score = 0
+        speed = 1
+        bird.x = 50
+        bird.y = HEIGHT/2
+        bar_up.x = WIDTH
+        bar_up.y = 0
+        bar_down.x = WIDTH
+        bar_down.y = 600
 
 def on_mouse_down():
     bird.y = bird.y - 100
